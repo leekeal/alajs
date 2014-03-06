239752cfc081d9f8377a25c7890856d659fee250
leeke <leeke@ala-team.com>
2014-03-06 10:44:25 +0900

    edit

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ var handlers = require("./lib/globalHandler"); /*开启全局工具*/
 
 /*------------------- 占用全局alajs命名  -----------------*/
 GLOBAL.alajs = {
-	bootstrap:function(appName){
+	bootstrap:function(appName,numCPUs){
 		handlers();/*加载handlers*/
-		var core = new Core(appName);
-		core.bootstrap();
-		return core;
-
+		return create(appName,numCPUs);
 	},
 
 
@@ -19,4 +16,32 @@ GLOBAL.alajs = {
 
 module.exports = global.alajs;
 
+
+
+
+
+var cluster = require('cluster');
+function create(appName,numCPUs){
+	if (!numCPUs){
+		var numCPUs = require('os').cpus().length;
+	}
+
+	if (cluster.isMaster) {
+  		// Fork workers.
+  		for (var i = 0; i < numCPUs; i++) {
+  			cluster.fork();
+  		}
+
+  		cluster.on('exit', function(worker, code, signal) {
+  			console.log('worker ' + worker.process.pid + ' died');
+  		});
+  	} 
+  	else {
+  		var core = new Core(appName);
+		app = core.bootstrap();
+		GLOBAL.app =  core;
+
+  	}
+
+  }
 
